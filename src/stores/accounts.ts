d6865b6b858dcd7f6f419390b29ed51476f36118
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-30 14:56:27 +0300

    feat: add Pinia store for accounts management

---
+++ src/stores/accounts.ts
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+
+type AccountType = 'LDAP' | 'Локальная'
+
+type Account = {
+    id: string
+    labels: { text: string }[]
+    type: AccountType
+    login: string
+    password: string | null
+}
+
+
+export const useAccountsStore = defineStore('accounts', {
+    state: () => ({
+        accounts: [] as Account[],
+    }),
+    actions: {
+        addAccount() {
+            this.accounts.push({
+                id: crypto.randomUUID(),
+                labels: [],
+                type: 'Локальная',
+                login: '',
+                password: ''
+            })
+        },
+        removeAccount(id: string) {
+            this.accounts = this.accounts.filter(a => a.id !== id)
+        },
+        updateAccount(updated: Account) {
+            const index = this.accounts.findIndex(a => a.id === updated.id)
+            if (index !== -1) this.accounts[index] = updated
+        }
+    },
+    // persist: true
+})
