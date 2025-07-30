f231bd8bc018e25bd698cb9b5487b29aa52a6804
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-30 14:25:25 +0300

    chore: setup ElementPlus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
+const dialogVisible = ref(false)
+
+const showDialog = () => {
+  dialogVisible.value = true
+}
+
+const handleConfirm = () => {
+  alert('Действие подтверждено!')
+  dialogVisible.value = false
+}
+</script>
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="app">
+    <el-button type="primary" @click="showDialog">Открыть</el-button>
+    
+    <el-dialog v-model="dialogVisible" title="Подтверждение">
+      <p>Вы уверены?</p>
+      <template #footer>
+        <el-button @click="dialogVisible = false">Отмена</el-button>
+        <el-button type="primary" @click="handleConfirm">Подтвердить</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  padding: 20px;
+}
+</style>
