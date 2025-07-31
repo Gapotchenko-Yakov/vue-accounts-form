556e17d64c8d0e210bf4c806bd06ffca6e15fe51
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-31 21:18:42 +0300

    chore: change invalidLabelsFormat validation error message

--- src/const/validationErrors.ts
+++ src/const/validationErrors.ts
@@ -1,6 +1,8 @@
+import { LABELS_DELIMITER } from "./accounts";
+
 export const VALIDATION_ERRORS = {
     required: 'Обязательное поле',
     maxLength: (n: number) => `Максимум ${n} символов`,
-    invalidLabelsFormat: 'Метки должны быть разделены точкой с запятой и не содержать пустых значений',
+    invalidLabelsFormat: `Метки должны быть разделены '${LABELS_DELIMITER}' и не содержать пустых значений`,
     invalidType: 'Недопустимый тип записи',
 }
